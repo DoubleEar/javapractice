0631ae92b985dabe396a5267cd2f268c334f51b2
DoubleEar <1638435068@qq.com>
2019-10-27 12:07:52 +0800

    输入若干数字求平均值

---
+++ java变量-语句/JavaTest.java
@@ -0,0 +1,43 @@
+public class JavaTest{
+	public static void main(String[] args){
+		/*System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args.length);
+		String action="你好";
+		for(int i=0;i<args.length;i++){
+			System.out.println(action+"\""+args[i]+"\"");
+		}
+		 if(args.length<2){
+			System.out.println("请输入两个数");
+			return;
+		 }
+            int x=Integer.parseInt(args[0]);
+			int y=Integer.parseInt(args[1]);
+			int result=x+y;
+			System.out.println(
+			""+ x + " + " + y + "相加的结果是" + result
+			);
+			System.out.printf("%d + %d相加的结果是%d %n",x,y,result);
+			System.out.print(""+ x + " + " + y + "相加的结果是" + result);
+			*/
+			
+		/* String x="我是字符串";
+		 System.out.println(x);
+		 {
+			 int x=10;
+			 System.out.println(x);
+		 } 错误代码*/  
+		 /*String s="123456";
+		 System.out.println(Integer.parseInt(s));
+		 System.out.println(Long.parseLong(s));
+		 System.out.println(Double.parseDouble(s));
+		 int x=20;
+		 long y=199l;
+		 double z=3.1415;
+		 System.out.println(String.valueOf(x));
+		 System.out.println(String.valueOf(y));
+		 System.out.println(String.valueOf(z));
+		 */
+		 }
+}
+		
