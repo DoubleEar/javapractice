bad2ba8969ec2c3fe15364761dad9a7a55575cea
DoubleEar <1638435068@qq.com>
2019-10-28 15:06:32 +0800

    交换两个变量的值

---
+++ java变量-语句/ExchangeValue.java
@@ -0,0 +1,15 @@
+public class ExchangeValue{
+	public static void main(String[] args){
+		int a =Integer.parseInt(args[0]);
+		int b =Integer.parseInt(args[1]);
+		int tmp=0;
+		tmp =a;  
+		b=tmp;
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+}
+		
+	
+		
