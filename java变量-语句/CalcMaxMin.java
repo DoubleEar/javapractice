342c274006994c3c8b06f1ac3b8bbb0af19bcaca
DoubleEar <1638435068@qq.com>
2019-10-28 15:05:37 +0800

    计算三个数值的最大最小值

---
+++ java变量-语句/CalcMaxMin.java
@@ -0,0 +1,14 @@
+public class CalcMaxMin{
+	public static void main(String[] args){
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		int c=Integer.parseInt(args[2]);
+        int max=(a>b)?(a>c?a:c):(b>c?b:c);
+        int min=(a<b)?(a<c?a:c):(b<c?b:c);
+		System.out.println(max);
+		System.out.println(min);
+		
+	}
+}
+			
+			
