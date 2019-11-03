239d284d0dd57a653cb6431b71f94026c5b41da3
DoubleEar <1638435068@qq.com>
2019-11-03 21:58:06 +0800

    方法与递归的应用

---
+++ 方法与递归/RecursionDemo.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class RecursionDemo{
+	/*public static int sum(int n){
+		if(n<10){
+			return n;
+		}else{
+			return sum(n/10)+(n%10);
+		}
+	}
+	*/
+	//0 1 2
+	public static void hanoi(int n,int from,int to){
+		if(n==1){
+			System.out.printf("从%d移动到%d\n",from,to);
+		}else{
+			int other=3-from-to;
+			hanoi(n-1,from,other);
+			System.out.printf("从%d移动到%d\n",from,to);
+			hanoi(n-1,other,to);
+			System.out.printf("从%d移动到%d\n",from,to);
+		}
+	}
+	public static void main(String[] args){
+		/*Scanner scanner=new Sc
+		anner(System.in);
+		int input=scanner.nextInt();
+		System.out.println(sum(input));*/
+		hanoi(4,0,2);
+	}
+}
+	
+		
