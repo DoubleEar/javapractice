5978f22f978baba2e6710a14d7fe8abb433c1572
DoubleEar <1638435068@qq.com>
2019-11-09 22:46:36 +0800

    变量的温习

---
+++ java变量-语句/VariableDemo.java
@@ -0,0 +1,63 @@
+public class VariableDemo{
+	public static void main(String[]args){
+		int a=3;
+		System.out.println(a);
+		System.out.println("----------------");
+		byte minByte=Byte.MIN_VALUE;
+		byte maxByte=Byte.MAX_VALUE;
+		System.out.println(minByte);
+		System.out.println(maxByte);
+		System.out.println("----------------");
+		short minShort=Short.MIN_VALUE;
+		short maxShort=Short.MAX_VALUE;
+		System.out.println(minShort);
+		System.out.println(maxShort);
+		System.out.println("----------------");
+		short minShortSub1=(short)(minShort-1);
+		System.out.println(minShortSub1);
+		short maxShortAdd1=(short)(maxShort+1);
+		System.out.println(maxShortAdd1);
+		System.out.println("----------------");
+		long minLong=Long.MIN_VALUE;
+		long maxLong=Long.MAX_VALUE;
+		System.out.println(minLong);
+		System.out.println(maxLong);
+		System.out.println("----------------");
+		int minInt=Integer.MIN_VALUE;
+		int maxInt=Integer.MAX_VALUE;
+		System.out.println(minInt);
+		System.out.println(maxInt);
+		System.out.println("----------------");
+		int bInt=0b1001;
+		System.out.println(bInt);
+		short cShort=0x4D;
+		System.out.println(cShort);
+		long aLong=10L;
+		System.out.println(aLong);
+		System.out.println("----------------");
+		int aInt=1;
+		int cInt=2;
+		System.out.println(aInt/cInt);
+		System.out.println(aInt*1.0/cInt);
+		System.out.println("----------------");
+		char aChar;
+	    aChar='A';
+		System.out.println(aChar);
+		char bChar;
+		bChar='鹏';
+		System.out.println(bChar);
+		char cChar;
+		cChar='♦';
+		System.out.println(cChar);
+	}
+}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
