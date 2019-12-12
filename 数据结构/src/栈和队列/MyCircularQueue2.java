37640f064cbe9f6fd7866ff37856c1a42f2cc84c
DoubleEar <1638435068@qq.com>
2019-12-12 18:46:42 +0800

    用顺序表实现循环队列（用size纪录）

---
+++ 数据结构/src/栈和队列/MyCircularQueue2.java
@@ -0,0 +1,67 @@
+package 栈和队列;
+//通过增加size属性纪录
+public class MyCircularQueue2 {
+    int size;
+    int[] array;
+    int rear;
+    int front;
+    public MyCircularQueue2(int k){
+         array=new int[k+1];
+         size=0;
+         rear=front=0;
+    }
+    //入队
+    public boolean enQueue(int value) {
+        if (isFull()) {
+            return false;
+        }
+        array[rear++] = value;
+        size++;
+        //判断位置是否越界，更新位置
+        //rear=rear%array.length;
+        if (rear == array.length) {
+            rear = 0;
+        }
+        return true;
+    }
+
+    //出队
+    public boolean deQueue(){
+        if(isEmpty()){
+            return  false;
+        }
+        front++;
+        front=front%array.length;
+        size--;
+        return true;
+    }
+
+    //获得队头元素
+    public int Front(){
+        if(isEmpty()){
+            return -1;
+        }
+        return array[front];
+    }
+
+    //获得队尾元素
+    public int rear(){
+        if(isEmpty()){
+            return -1;
+        }
+        if(rear==0){
+            return array[array.length-1];
+        }
+        return array[rear-1];
+    }
+
+    //判断是否为空
+    public boolean isEmpty(){
+        return size==0;
+    }
+
+    //判断是否满了
+    public boolean isFull(){
+        return size==array.length-1;
+    }
+}
