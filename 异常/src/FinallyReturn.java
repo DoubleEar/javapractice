02e2268afc807cc51179114ea4e685aa8d0c4c0f
DoubleEar <1638435068@qq.com>
2019-12-13 23:05:25 +0800

    受查异常、finally等

---
+++ 异常/src/FinallyReturn.java
@@ -0,0 +1,19 @@
+public class FinallyReturn {
+    //无论怎样都要执行
+    private static int method(){
+        try{
+            System.out.println("1");
+            throw new RuntimeException();
+        }catch (RuntimeException e){
+            System.out.println("2");
+            return 20;
+        }finally {
+            System.out.println("3");
+            return 30;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(method());
+    }
+}
