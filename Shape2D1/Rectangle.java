da9385154a50104e6ef25a7cc1605bc4fd337935
DoubleEar <1638435068@qq.com>
2019-12-25 11:42:44 +0800

    Rectangle类实现接口，覆写方法。

---
+++ Shape2D1/Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle implements Shape2D{
+	int width;
+	int height;
+public Rectangle(int w ,int h) {
+	this.width=w;
+	this.height=h;
+}
+public double area(){
+  return (h*w);
+  }
+}
