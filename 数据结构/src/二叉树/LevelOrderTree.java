b55c28196827acf933ff189cb2e7477e158c5e4c
DoubleEar <1638435068@qq.com>
2019-12-26 14:32:37 +0800

    层序遍历二叉树，将根存入list

---
+++ 数据结构/src/二叉树/LevelOrderTree.java
@@ -0,0 +1,32 @@
+package 二叉树;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrderTree {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> list = new LinkedList<>();
+        Queue<Node> queue = new LinkedList<>();
+        if (root != null)
+            queue.offer(root);
+        while (!queue.isEmpty()) {
+            //取出队列所有元素
+            int size = queue.size();
+            List<Integer> rowNode = new ArrayList<>();
+            while (size > 0) {
+                Node topNode = queue.poll();
+                //放在同一行
+                rowNode.add(topNode.value);
+                if (topNode.left != null)
+                    queue.offer(topNode.left);
+                if (topNode.right != null)
+                    queue.offer(topNode.right);
+                size--;
+            }
+            list.add(rowNode);
+        }
+        return list;
+    }
+}
