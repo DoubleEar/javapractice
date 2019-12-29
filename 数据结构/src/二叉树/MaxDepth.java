5d931c983e7c5a649a675387794f964edb47c436
DoubleEar <1638435068@qq.com>
2019-12-29 17:09:21 +0800

    求一棵树的最大深度

---
+++ 数据结构/src/二叉树/MaxDepth.java
@@ -0,0 +1,13 @@
+package 二叉树;
+
+//返回树的最大深度
+public class MaxDepth {
+    public int maxDepth(Node root){
+        //return root==null?0:Math.max(maxDepth(root.right),maxDepth(root.left))+1;
+        if(root==null)
+            return 0;
+        int left=maxDepth(root.left);
+        int right=maxDepth(root.right);
+        return left>right?1+left:1+right;
+    }
+}
