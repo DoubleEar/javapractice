28b9977509aae1f4e30ae72afa4c309152be90c8
DoubleEar <1638435068@qq.com>
2020-01-05 22:14:20 +0800

    Node

---
+++ 数据结构/src/二叉树/Node.java
@@ -0,0 +1,13 @@
+package 二叉树;
+
+//二叉链
+class Node {
+    int value;
+    Node left;
+    Node right;
+
+    public Node(int value) {
+        this.value = value;
+        left = right = null;
+    }
+}
