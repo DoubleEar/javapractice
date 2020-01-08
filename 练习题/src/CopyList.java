055428bc3e24ac4f568a7319e9009278713a10ac
DoubleEar <1638435068@qq.com>
2020-01-08 23:47:45 +0800

    简单的链表复制

---
+++ 练习题/src/CopyList.java
@@ -0,0 +1,34 @@
+public class CopyList {
+    public static void printLinkedList(Node head){
+        Node cur=head;
+        while(cur!=null){
+            System.out.println(cur.val);
+            cur=cur.next;
+        }
+    }
+    public static Node copy(Node head){
+        Node nHead=null;
+        Node nLast=null;
+
+        Node cur=head;
+        while(cur!=null){
+            Node node=new Node(cur.val);
+            if(nHead==null){
+                nHead=node;
+            }else{
+                nLast.next=node;
+            }
+            nLast=node;
+
+            cur=cur.next;
+        }
+        return nHead;
+    }
+    public static void main(String[] args){
+        Node n3=new Node(6);
+        Node n2=new Node(3,n3);
+        Node n1=new Node(2,n2);
+
+        printLinkedList(copy(n1));
+    }
+}
--- 练习题/src/Rotate.java
+++ 练习题/src/Rotate.java
@@ -1,3 +1,4 @@
+//给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
 public class Rotate {
     public void rotate(int[] nums, int k) {
         if(k==0||nums.length<2){
