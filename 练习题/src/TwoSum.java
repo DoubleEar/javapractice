556763c511400cb6e11dadfaf9b6ab3050e1ec7e
DoubleEar <1638435068@qq.com>
2020-01-23 20:56:34 +0800

    在该数组中找出和为目标值的那两个整数，并返回他们的数组下标

---
+++ 练习题/src/TwoSum.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+//给定一个整数数组 nums 和一个目标值 target，
+// 请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+        int []nums={1,2,3,4,5};
+        System.out.println(Arrays.toString(twoSum(nums,6)));
+    }
+}
