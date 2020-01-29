b13a8a488f038549c55a51e32e2fbc053a02421c
DoubleEar <1638435068@qq.com>
2020-01-29 21:06:26 +0800

    根据逆波兰表示法，求表达式的值。

---
+++ 练习题/src/EvalRPN.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+//有效的运算符包括 +, -, *, / 。每个运算对象可以是整数
+// 也可以是另一个逆波兰表达式。
+public class EvalRPN {
+    public static int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String s : tokens) {
+            if (s.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } else if (s.equals("-")) {
+                stack.push(-stack.pop() + stack.pop());
+            } else if (s.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (s.equals("/")) {
+                int num1 = stack.pop();
+                stack.push(stack.pop() / num1);
+            } else {
+                stack.push(Integer.parseInt(s));
+            }
+        }
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+        String[] s={"4", "13", "5", "/", "+"};
+        System.out.println(evalRPN(s));
+    }
+}
