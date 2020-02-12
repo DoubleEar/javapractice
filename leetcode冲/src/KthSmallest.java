efa59fc038f7252dd16ddb91f16fb76fa3a59dc6
DoubleEar <1638435068@qq.com>
2020-02-12 19:53:51 +0800

    找到有序矩阵中第k个小的数

---
+++ leetcode冲/src/KthSmallest.java
@@ -0,0 +1,50 @@
+//找到有序矩阵中第k个小的数
+public class KthSmallest {
+    public static int kthSmallest(int[][] matrix, int k) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int left = matrix[0][0];
+        int right = matrix[row - 1][col - 1];
+        while (left < right) {
+            // 每次循环都保证第k小的数在left~rtght之间，当left==right，第k小的数就是right
+            int mid = (left + right) / 2;
+            // 找二维矩阵中<=mid的元素总个数
+            int count = findNotBiggerThanMid(matrix, mid, row, col);
+            if (count < k) {
+                // 第k小的数在右半部分，且不包含mid
+                left = mid + 1;
+            } else {
+                // 第k小的数在左半部分，可能包含mid
+                right = mid;
+            }
+        }
+        return right;
+    }
+
+    private static int findNotBiggerThanMid(int[][] matrix, int mid, int row, int col) {
+        // 以列为单位找，找到每一列最后一个<=mid的数即知道每一列有多少个数<=mid
+        int i = row - 1;
+        int j = 0;
+        int count = 0;
+        while (i >= 0 && j < col) {
+            if (matrix[i][j] <= mid) {
+                // 第j列有i+1个元素<=mid
+                count += i + 1;
+                j++;
+            } else {
+                // 第j列目前的数大于mid，需要继续在当前列往上找
+                i--;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix={
+                {1,5,9},
+                {10,11,13},
+                {12,13,15}};
+        System.out.println(kthSmallest(matrix,4));
+
+    }
+}
