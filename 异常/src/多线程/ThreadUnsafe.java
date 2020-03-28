152c8c6985efde835eb10572ae77debf2f724025
DoubleEar <1638435068@qq.com>
2020-03-28 23:14:09 +0800

    线程不安全——破坏了原子性

---
+++ 异常/src/多线程/ThreadUnsafe.java
@@ -0,0 +1,36 @@
+package 多线程;
+
+//线程不安全示例——原子性被破坏了。
+public class ThreadUnsafe {
+    private static long n=0;
+    static class Add extends Thread{
+        @Override
+        public void run() {
+            for(int i=0;i<100000;i++){
+                n++;
+            }
+        }
+    }
+
+    static class Sub extends Thread{
+        @Override
+        public void run() {
+            for(int i=0;i<100000;i++){
+                n--;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Add add=new Add();
+        add.start();
+        Sub sub=new Sub();
+        sub.start();
+
+        add.join();
+        sub.join();
+
+        System.out.println(n);
+
+    }
+}
