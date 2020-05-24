b98af6e0fd1afb54e9c0c294347e8a896ebf5647
DoubleEar <1638435068@qq.com>
2020-05-24 23:25:20 +0800

    规定、标准

---
+++ 自己实现Tomcat/src/standard/HttpServletRequest.java
@@ -0,0 +1,12 @@
+package standard;
+
+public interface HttpServletRequest extends ServletRequest {
+    String getMethod();
+
+    String getPath();
+
+    String getParameter(String name);
+
+    String getHeader(String name);
+
+}
