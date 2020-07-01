11bbaf0315c7ad7523b9791600bc37ab8436e26b
DoubleEar <1638435068@qq.com>
2020-07-01 23:23:17 +0800

    数据库设计--数据表的建立

--- 剑指Offer/src/NthUglyNumber.java
+++ 剑指Offer/src/NthUglyNumber.java
@@ -2,12 +2,15 @@
 //我们把只包含质因子 2、3 和 5 的数称作丑数。求按从小到大的顺序的第 n 个丑数。
 public class NthUglyNumber {
     /*
-        丑数只包含因子2，3，5，因此有：丑数=某较小丑数*某因子。（10=2*5）
+        丑数只包含因子2，3，5（丑数只能被2，3，5整除），因此有：丑数=某较小丑数*某因子。（10=2*5）
         Xa为首个乘以2后大于Xn的丑数     Xb为首个乘以3后大于Xn的丑数    Xc为首个乘以5后大于Xn的丑数
         下一个丑数Xn+1=Math.min(Xa*2,Xb*3,Xc*5)。
      */
     public static int nthUglyNumber(int n) {
         int a=0,b=0,c=0;    //先设置a,b,c，指向第一个丑数1。
+                            // a指向*2的得到的丑数的某一个下标。
+                            // b指向*3的得到的丑数的某一个下标。
+                            // c指向*5的得到的丑数的某一个下标。
         int[] dp=new int[n];
         dp[0]=1;
 
@@ -16,11 +19,11 @@ public class NthUglyNumber {
             dp[i]=Math.min(Math.min(n2,n3),n5);
             //独立判断dp[i]与dp[a]*2,dp[b]*3,dp[c]*5的大小关系，更新索引。
             //若相等，对应的索引加1。
-            if(dp[i]==n2)
+            if(dp[i]==n2)   //每次生成新的丑数n2，更新a。
                 a++;
-            if (dp[i]==n3)
+            if (dp[i]==n3)  //每次生成新的丑数n3，更新b。
                 b++;
-            if(dp[i]==n5)
+            if(dp[i]==n5)   //每次生成新的丑数n4，更新c。
                 c++;
         }
         return dp[n-1];
