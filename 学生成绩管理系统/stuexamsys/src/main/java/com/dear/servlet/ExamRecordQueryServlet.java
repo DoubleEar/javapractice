3fb69a7ab7bdefaf14a91890e9f2d8c3acce4565
DoubleEar <1638435068@qq.com>
2020-07-01 23:25:57 +0800

    成绩查询的servlet处理

---
+++ 学生成绩管理系统/stuexamsys/src/main/java/com/dear/servlet/ExamRecordQueryServlet.java
@@ -0,0 +1,19 @@
+package com.dear.servlet;
+//成绩查询的servlet处理
+import com.dear.dao.ExamRecordDAO;
+import com.dear.model.ExamRecord;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@WebServlet("/examRecord/query")
+public class ExamRecordQueryServlet extends AbstractBaseServlet{
+
+    @Override
+    public Object process(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        List<ExamRecord> records=ExamRecordDAO.query();
+        return records;
+    }
+}
