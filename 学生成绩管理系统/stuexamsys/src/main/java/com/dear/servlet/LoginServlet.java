8f7d8efe07f3c88841a4383fa0bf152199ba7c2c
DoubleEar <1638435068@qq.com>
2020-07-03 23:23:32 +0800

    存储数据

--- 学生成绩管理系统/stuexamsys/src/main/java/com/dear/servlet/LoginServlet.java
+++ 学生成绩管理系统/stuexamsys/src/main/java/com/dear/servlet/LoginServlet.java
@@ -1,5 +1,5 @@
 package com.dear.servlet;
-
+//用户登录的servlet处理
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
