d88b707ab29831d92ddfbb01debdc7f01c06eb06
DoubleEar <1638435068@qq.com>
2020-07-06 23:56:27 +0800

    定义AVL树的节点及属性

---
+++ 数据结构/src/AVL树/Node.java
@@ -0,0 +1,21 @@
+package AVL树;
+
+public class Node {
+    int key;
+    //平衡因子
+    int bf;
+
+    Node left;
+    Node right;
+
+    //记录结点的父结点，如果结点是根结点，则 parent == null
+    Node parent;
+
+    Node(int key,Node parent){
+        this.key=key;
+        this.parent=parent;
+        this.bf=0;
+        this.left=null;
+        this.right=null;
+    }
+}
