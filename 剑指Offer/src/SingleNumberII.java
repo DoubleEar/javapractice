6b8f186a1c0f6c11fa1ea699ba2a0ef7ba948706
DoubleEar <1638435068@qq.com>
2020-07-15 23:54:35 +0800

    数组中数字出现的次数 II

---
+++ 剑指Offer/src/SingleNumberII.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+//数组中数字出现的次数 II
+public class SingleNumberII {
+    /*
+    //方法一：哈希表
+    public int singleNumber(int[] nums) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+
+        for(int num:nums){
+            if(map.get(num)==1)
+                return num;
+        }
+        return -1;
+    }
+    */
+}
