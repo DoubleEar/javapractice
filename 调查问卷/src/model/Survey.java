5bf2d5941ffdb6ab90b9b0bdc2d5fd5b63c1873b
DoubleEar <1638435068@qq.com>
2020-08-01 23:48:57 +0800

    问卷数据

---
+++ 调查问卷/src/model/Survey.java
@@ -0,0 +1,43 @@
+package model;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class Survey {
+    public Integer sid;
+    public User user;
+    public String title;
+    public String brief;
+    public String coverUrl;
+    public String thanks;
+
+    public static class JSONUtil implements JSONSerializable<Survey>{
+
+        @Override
+        public JSONObject toJSON(Survey model) {
+            JSONObject object=new JSONObject();
+            if(model.sid!=null){
+                object.put("sid",model.sid);
+            }
+            if(model.title!=null){
+                object.put("title",model.title);
+            }
+            if(model.brief!=null){
+                object.put("brief",model.brief);
+            }
+            if(model.coverUrl!=null){
+                object.put("coverUrl",model.coverUrl);
+            }
+            if(model.user!=null){
+                object.put("user",User.jsonUtil.toJSON(model.user));
+            }
+            return object;
+        }
+
+        @Override
+        public Survey fromJSON(JSONObject object) {
+            return null;
+        }
+    }
+
+    public static final JSONUtil jsonUtil=new JSONUtil();
+}
