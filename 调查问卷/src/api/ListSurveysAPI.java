29bcd7228e5a0748779d7a8efeebbafaf7b8b1ee
DoubleEar <1638435068@qq.com>
2020-08-02 23:14:31 +0800

    获取问卷列表的API

---
+++ 调查问卷/src/api/ListSurveysAPI.java
@@ -0,0 +1,29 @@
+package api;
+
+import com.alibaba.fastjson.JSON;
+import model.Survey;
+import service.SurveyService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet("/API/list-surveys.json")
+public class ListSurveysAPI extends AbsHttpServletAPI{
+    private SurveyService surveyService;
+
+    @Override
+    public void init() throws ServletException {
+        surveyService=new SurveyService();
+    }
+
+    @Override
+    JSON getData(HttpServletRequest req, HttpServletResponse resp) throws SQLException {
+        List<Survey> surveyList=surveyService.list();
+
+        return Survey.jsonUtil.toJSON(surveyList);
+    }
+}
