2f03e00c1ec1c2307f2b075e5a2d20b242dcd38c
DoubleEar <1638435068@qq.com>
2020-08-03 22:28:25 +0800

    页面显示

--- 调查问卷/src/api/ListSurveysAPI.java
+++ 调查问卷/src/api/ListSurveysAPI.java
@@ -3,7 +3,7 @@ package api;
 import com.alibaba.fastjson.JSON;
 import model.Survey;
 import service.SurveyService;
-
+//将调查问卷列表数据转化为API格式的规定
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
